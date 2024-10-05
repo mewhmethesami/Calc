24cad334d60e5d34c3b81ae2f96288c2a2fc6436
MohammadReza <mewhmethesami1999@gmail.com>
2024-10-05 14:26:14 +0330

    create Calc

---
+++ scrippt.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("display")
+
+function appendToDisplay(input) {
+    display.value = display.value + input
+}
+
+function clearDisplay() {
+    display.value = ''
+}
+
+function calculate() {
+    display.value = eval(display.value)
+}
